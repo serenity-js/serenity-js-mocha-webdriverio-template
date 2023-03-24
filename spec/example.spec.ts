cd38e146b84c66f56a06e10a90156f8012fd5d84
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-03-24 16:41:06 +0000

    Updated the test

--- spec/example.spec.ts
+++ spec/example.spec.ts
@@ -1,16 +1,13 @@
 import { Ensure, includes } from '@serenity-js/assertions';
 import { actorCalled } from '@serenity-js/core';
-import { By, Click, Navigate, Page, PageElement } from '@serenity-js/web';
+import { By, Navigate, Page, PageElement } from '@serenity-js/web';
 import { describe, it } from 'mocha';
 
 describe('serenity-js.org website', () => {
 
-    const acceptCookiesButton = PageElement.located(By.css('.cookieinfo-close')).describedAs('accept cookies button')
-
     it(`tells people what they're reading`, () =>
         actorCalled('Alice').attemptsTo(
             Navigate.to('https://serenity-js.org'),
-            Click.on(acceptCookiesButton),
             
             Ensure.that(Page.current().title(), includes('Serenity/JS')),
         ));
